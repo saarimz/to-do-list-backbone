df68737c4b41a0d1e9b9f72d7503a151da0a3b9b
Saarim Zaman <saarim.z@gmail.com>
2017-09-28 14:59:37 -0400

    set up basics

---
+++ js/app.js
@@ -0,0 +1,16 @@
+let ToDoItem = Backbone.Model.extend({
+
+});
+
+let ToDoItems = Backbone.Collection.extend({
+
+});
+
+let ToDoItemView = Backbone.View.extend({
+
+});
+
+let ToDoList = Backbone.View.extend({
+
+});
+
