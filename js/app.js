2573489c883db9f7cfd4fd8b1fc1a8738547dbb8
Saarim Zaman <saarim.z@gmail.com>
2017-10-02 22:56:25 -0400

    added core functionality

--- js/app.js
+++ js/app.js
@@ -1,4 +1,11 @@
+let data = [
+    { id: 1, name: "Eat apples"},
+    { id: 2, name: "Drink water"},
+    { id: 3, name: "Get groceries"}
+];
+
 let ToDoItem = Backbone.Model.extend({
+    idAttribute: 'id',
     defaults: {
         name: '',
         timestamp: new Date(Date.now()),
@@ -6,9 +13,11 @@ let ToDoItem = Backbone.Model.extend({
     },
 
     toggle: function(){
-        this.save({
-            completed: !this.get('completed')
-        });
+        if (this.get('completed')) {
+            this.set('completed',false);
+        } else {
+            this.set('completed',true);
+        }
     }
 });
 
@@ -19,25 +28,40 @@ let ToDoItems = Backbone.Collection.extend({
 let ToDoItemView = Backbone.View.extend({
 
     tagName: 'li',
+    className: 'list-item',
 
     events: {
-        'click .delete-button' : 'submit'
+        'click .delete-button' : 'delete',
+        'click #item-name' : 'strike' 
     },
 
-    template: _.template("#to-do-item"),
-
-    init: function(){
-        this.render();
+    initialize: function(){
+        let self = this;
+        self.model.on("change",function(item){
+            if (item.get("completed")) {
+                self.$el.find("#item-name").addClass("completed");
+            }
+            else {
+                self.$el.find("#item-name").removeClass("completed");
+            }
+        },self);
     },
+
+    template: _.template($("#to-do-item").html()),
     
     render: function(){
         let self = this;
-        this.$el.html(this.template(this.model.toJSON()));
+        let html = self.template(self.model.toJSON());
+        self.$el.html(html);
         return self;
     },
 
-    submit: function(){
-        console.log("Button pressed!");
+    delete: function(){
+        this.remove();
+    },
+
+    strike: function(){
+        this.model.toggle();
     }
 });
 
@@ -46,12 +70,41 @@ let ToDoList = Backbone.View.extend({
     el: ".main-list",
 
     initialize: function(){
+       this.model.on("remove", this.removeItem, this);
+       this.model.on("add",this.addItem, this);
+    },
+
+    removeItem: function(item){
+        this.$("li#"+item.id).remove();
+    },
+
+    addItem: function(item){
         this.render();
     },
 
     render: function(){
-        console.log("View Loaded");
-        return this;
+        let self = this;
+        self.$el.html("");
+        this.model.each(function(item){
+            let itemView = new ToDoItemView({model: item});
+            self.$el.append(itemView.render().$el);
+        })
+        return self;
     }
 }); 
 
+
+//instantiate
+let toDoItems = new ToDoItems(data.map(function(item){
+    return new ToDoItem(item);
+}));
+
+let toDoList = new ToDoList({model: toDoItems});
+toDoList.render();
+
+//TO DO: Convert this to backbone.js from jquery
+$("#add-button").click(function(e){
+    e.preventDefault();
+    let input = $("#list-input").val();
+    toDoItems.add(new ToDoItem({name: input}));
+});
